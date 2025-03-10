494b9bc1b2b9f729597d112d96f82a17b12420e9
Amy He <ayhe1768@gmail.com>
2025-03-10 16:01:45 -0700

    make remark mutable in h

--- src/lib/vina.h
+++ src/lib/vina.h
@@ -144,7 +144,7 @@ public:
 	std::string get_poses(int how_many=9, double energy_range=3.0);
 	std::vector< std::vector<double> > get_poses_coordinates(int how_many=9, double energy_range=3.0);
 	std::vector< std::vector<double> > get_poses_energies(int how_many=9, double energy_range=3.0);
-	void write_pose(const std::string &output_name, const std::string &remark = std::string());
+	void write_pose(const std::string &output_name, std::string &remark = std::string());
 	void write_poses(const std::string &output_name, int how_many=9, double energy_range=3.0);
 	void write_maps(const std::string& map_prefix="receptor", const std::string& gpf_filename="NULL",
 					    const std::string& fld_filename="NULL", const std::string& receptor_filename="NULL");
